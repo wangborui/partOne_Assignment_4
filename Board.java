a690f544c5e08cf1bdc25bcfd24a7fec71748977
Borui Wang <wangbd@bc.edu>
2016-06-15 11:15:16 -0500

    Finishing Initialization of Board and Solver

--- Board.java
+++ Board.java
@@ -10,5 +10,46 @@ package partOne_Assignment_4;
  * @author Borui Wang
  */
 public class Board {
-    
+    public Board(int[][] blocks)           // construct a board from an N-by-N array of blocks
+                                           // (where blocks[i][j] = block in row i, column j)
+    {
+        
+    }
+    public int dimension()                 // board dimension N
+    {
+        return -1;
+    }
+    public int hamming()                   // number of blocks out of place
+    {
+        return -1;
+    }
+    public int manhattan()                 // sum of Manhattan distances between blocks and goal
+    {
+        return -1;
+    }
+    public boolean isGoal()                // is this board the goal board?
+    {
+        return false;
+    }
+    public Board twin()                    // a board that is obtained by exchanging any pair of blocks
+    {
+        return null;
+    }
+    public boolean equals(Object y)        // does this board equal y?
+    {
+        return false;
+    }
+    public Iterable<Board> neighbors()     // all neighboring boards
+    {
+        return null;
+    }
+    public String toString()               // string representation of this board (in the output format specified below)
+    {
+        return null;
+    }
+
+    public static void main(String[] args) // unit tests (not graded)
+    {
+        
+    }
 }
---
+++ Solver.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package partOne_Assignment_4;
+
+/**
+ *
+ * @author Borui Wang
+ */
+public class Solver {
+    public Solver(Board initial)           // find a solution to the initial board (using the A* algorithm)
+    {
+        
+    }
+    public boolean isSolvable()            // is the initial board solvable?
+    {
+        return false;
+    }
+    public int moves()                     // min number of moves to solve initial board; -1 if unsolvable
+    {
+        return -1;
+    }
+    public Iterable<Board> solution()      // sequence of boards in a shortest solution; null if unsolvable
+    {
+        return null;
+    }
+    public static void main(String[] args) // solve a slider puzzle (given below)
+    {
+        
+    }
+}
